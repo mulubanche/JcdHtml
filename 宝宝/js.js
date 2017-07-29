fa763c134fd8200a744fde4e92d5a559402bc19d
molubanche <jiangchaodu324@163.com>
2017-07-30 00:31:48 +0800

    第一个版本
    临时文件

---
+++ 宝宝/js.js
@@ -0,0 +1,17 @@
+// alert('jiang mou ren');
+
+function buttonClick(){
+	alert('哈哈哈哈哈！');
+	alert('你个大傻瓜！');
+	alert('我想你了~~~');
+}
+
+function justNumber(){
+	var str = windonw.opener.document.getElementById('input_text_01').value;
+	if(isNaN(str)) {
+		alert('是数字！');
+	}
+	else{
+		alert('不是数字！');
+	}
+}
